cd8a546e9818ad7a08d3b4a263dacb99490df7e5
Daniela Bragança <danibraganca@gmail.com>
2025-02-02 10:51:06 -0300

    Integração com webhook.site

--- notificacoes.py
+++ notificacoes.py
@@ -1,6 +1,14 @@
+import requests
+
+webhook_url = 'https://webhook.site/e3680cd5-04d6-4077-870a-10ddc8c7b67e'
+
 def enviar_notificacao(novo_pet):
     print(f"Notificação: Novo pet disponível para adoção: {novo_pet['nome']}!")
     
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(webhook_url, json=novo_pet, headers=headers)
+    return response.status_code == 200
+
 if __name__ == "__main__":
     novo_pet = {'nome': 'Bobby', 'tipo': 'Cachorro'}
     enviar_notificacao(novo_pet)
